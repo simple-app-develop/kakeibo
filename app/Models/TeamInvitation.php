d3433894a7702bf237227cc945427971ac22298c
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-19 20:08:19 +0900

    refactor: モデルの各ファイルに日本語でコメントを記述しました。

--- app/Models/TeamInvitation.php
+++ app/Models/TeamInvitation.php
@@ -6,11 +6,19 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Jetstream\Jetstream;
 use Laravel\Jetstream\TeamInvitation as JetstreamTeamInvitation;
 
+/**
+ * チーム招待モデルクラス
+ * 
+ * このモデルは、Jetstreamのチーム招待モデルを拡張しており、
+ * アプリケーション内のチームへの招待に関するデータを表現します。
+ */
 class TeamInvitation extends JetstreamTeamInvitation
 {
     /**
-     * The attributes that are mass assignable.
+     * 代入可能な属性
      *
+     * この属性定義は、モデルのインスタンスを作成または更新する際に、一括で代入可能な属性を指定します。
+     * 
      * @var array<int, string>
      */
     protected $fillable = [
@@ -19,7 +27,9 @@ class TeamInvitation extends JetstreamTeamInvitation
     ];
 
     /**
-     * Get the team that the invitation belongs to.
+     * 招待が属するチームを取得します。
+     * 
+     * @return BelongsTo チームへの関連を示すBelongsToインスタンス
      */
     public function team(): BelongsTo
     {
