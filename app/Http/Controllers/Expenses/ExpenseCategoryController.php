a81dc6716b5af8e769c3885d8a4a85cc70f2e14e
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-20 22:43:00 +0900

    refactor: 依存注入をコンストラクタを通して行うように修正

--- app/Http/Controllers/Expenses/ExpenseCategoryController.php
+++ app/Http/Controllers/Expenses/ExpenseCategoryController.php
@@ -5,35 +5,48 @@ namespace App\Http\Controllers\Expenses;
 use App\Actions\Expenses\ExpenseCategory\CreateExpenseCategory;
 use App\Actions\Expenses\ExpenseCategory\GetExpenseCategoriesByTeam;
 use App\Http\Controllers\Controller;
-use App\Models\ExpenseCategory;
 use Illuminate\Http\Request;
 
 class ExpenseCategoryController extends Controller
 {
+    protected $createExpenseCategoryAction;
+    protected $getExpenseCategoriesByTeamAction;
+
+    public function __construct(
+        CreateExpenseCategory $createExpenseCategoryAction,
+        GetExpenseCategoriesByTeam $getExpenseCategoriesByTeamAction
+    ) {
+        $this->createExpenseCategoryAction = $createExpenseCategoryAction;
+        $this->getExpenseCategoriesByTeamAction = $getExpenseCategoriesByTeamAction;
+    }
+
     public function create()
     {
         return view('expenses.expense_categories.create');
     }
 
-    public function store(Request $request, CreateExpenseCategory $action)
+    public function store(Request $request)
     {
-        $teamId = auth()->user()->currentTeam->id;
-
         $data = $request->validate([
             'type' => 'required|in:income,expense',
             'name' => 'required|string|max:255',
             'description' => 'nullable|string|max:255',
         ]);
-        $data['team_id'] = $teamId;
+        $data['team_id'] = $this->getCurrentTeamId();
 
-        $action->create($data);
+        $this->createExpenseCategoryAction->create($data);
 
         return redirect()->route('expense-category-index')->with('status', 'Category created successfully!');
     }
 
-    public function index(GetExpenseCategoriesByTeam $action)
+    public function index()
     {
-        $categories = $action->getByTeam(auth()->user()->currentTeam->id);
+        $categories = $this->getExpenseCategoriesByTeamAction->getByTeam($this->getCurrentTeamId());
         return view('expenses.expense_categories.index', compact('categories'));
+    }
+
+    private function getCurrentTeamId()
+    {
+        return auth()->user()->currentTeam->id;
     }
 }
