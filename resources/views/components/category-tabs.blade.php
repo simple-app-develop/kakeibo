f41d5ed29537a1eedb7bf7f8c408f41d93dbf496
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-20 22:30:45 +0900

    fix: langを使用するように修正

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,9 +1,9 @@
 <div x-data="{ activeTab: '{{ $default ?? 'expense' }}' }">
     <div>
         <button x-on:click="activeTab = 'expense'" :class="{ 'bg-indigo-500 text-white': activeTab === 'expense' }"
-            class="px-4 py-2 text-sm">支出</button>
+            class="px-4 py-2 text-sm">{{ __('Expense') }}</button>
         <button x-on:click="activeTab = 'income'" :class="{ 'bg-indigo-500 text-white': activeTab === 'income' }"
-            class="px-4 py-2 text-sm">収入</button>
+            class="px-4 py-2 text-sm">{{ __('Income') }}</button>
     </div>
 
     <div x-show="activeTab === 'expense'">
