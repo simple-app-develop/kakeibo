4da42e999b351faebfc270006212dddcc97dcace
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-20 23:32:39 +0900

    fix: ラジオボタンのタップ可能領域を増やした

--- resources/views/components/radio-button.blade.php
+++ resources/views/components/radio-button.blade.php
@@ -3,5 +3,5 @@
 <div class="mt-4 flex items-center">
     <input id="{{ $id }}" type="radio" name="{{ $name }}" value="{{ $value }}"
         {{ $checked ? 'checked' : '' }} {!! $attributes->merge(['class' => 'form-radio h-5 w-5 text-indigo-600']) !!}>
-    <x-label :for="$id" :value="$label" class="ml-2" />
+    <label for="{{ $id }}" class="ml-2 cursor-pointer py-2 px-4 block w-full">{{ $label }}</label>
 </div>
