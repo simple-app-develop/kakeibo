417dd152e7fc14958fe51dc0120f970ef7570801
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-21 02:07:02 +0900

    refactor: scriptファイルを整理しCDNではなくファイルを配置しました

---
+++ public/script/expense-category-sotable.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const updateCategoryOrder = (elementId, reorderUrl) => {
+        const categoryList = document.getElementById(elementId);
+        if (!categoryList) return;
+
+        Sortable.create(categoryList, {
+            animation: 150,
+            onEnd: function () {
+                const order = [];
+                categoryList
+                    .querySelectorAll("[data-id]")
+                    .forEach(function (item) {
+                        order.push(item.dataset.id);
+                    });
+
+                // サーバーに新しい順序を送信
+                fetch(reorderUrl, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-CSRF-TOKEN": document
+                            .querySelector('meta[name="csrf-token"]')
+                            .getAttribute("content"),
+                    },
+                    body: JSON.stringify({
+                        order: order,
+                    }),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        if (data.message === "Order updated successfully") {
+                            console.log("Order updated on backend");
+                        } else {
+                            console.error(
+                                "Error updating order on backend:",
+                                data.message
+                            );
+                        }
+                    });
+            },
+        });
+    };
+
+    updateCategoryOrder("expenseCategoryList", "/expense-category/reorder");
+    updateCategoryOrder("incomeCategoryList", "/expense-category/reorder");
+});
--- resources/views/expenses/expense_categories/index.blade.php
+++ resources/views/expenses/expense_categories/index.blade.php
@@ -39,51 +39,6 @@
                         @endif
                     </x-slot>
                 </x-category-tabs>
-
-                <script>
-                    document.addEventListener("DOMContentLoaded", () => {
-
-                        const updateCategoryOrder = (elementId, reorderUrl) => {
-                            const categoryList = document.getElementById(elementId);
-                            if (!categoryList) return;
-
-                            Sortable.create(categoryList, {
-                                animation: 150,
-                                onEnd: function() {
-                                    const order = [];
-                                    categoryList.querySelectorAll('[data-id]').forEach(function(item) {
-                                        order.push(item.dataset.id);
-                                    });
-
-                                    // サーバーに新しい順序を送信
-                                    fetch(reorderUrl, {
-                                            method: 'POST',
-                                            headers: {
-                                                'Content-Type': 'application/json',
-                                                'X-CSRF-TOKEN': document.querySelector(
-                                                    'meta[name="csrf-token"]').getAttribute('content')
-                                            },
-                                            body: JSON.stringify({
-                                                order: order
-                                            })
-                                        })
-                                        .then(response => response.json())
-                                        .then(data => {
-                                            if (data.message === 'Order updated successfully') {
-                                                console.log('Order updated on backend');
-                                            } else {
-                                                console.error('Error updating order on backend:', data
-                                                    .message);
-                                            }
-                                        });
-                                }
-                            });
-                        };
-
-                        updateCategoryOrder('expenseCategoryList', '/expense-category/reorder');
-                        updateCategoryOrder('incomeCategoryList', '/expense-category/reorder');
-                    });
-                </script>
             </div>
         </div>
     </div>
