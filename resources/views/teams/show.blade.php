e33ac829eb960a3e19a1697c6471cc12bc2b1557
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-22 11:25:34 +0900

    fix: 設定から支払い方法へのリンクを作成

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -13,6 +13,12 @@
 
             @livewire('teams.category-setting', ['team' => $team])
 
+            <x-section-border />
+
+            @livewire('teams.payment-method', ['team' => $team])
+
+
+
             @livewire('teams.team-member-manager', ['team' => $team])
 
             @if (Gate::check('delete', $team) && !$team->personal_team)
