f0c2727cd2329addb8d5f91cfe1ca7460074da21
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-24 03:05:13 +0900

    feature: マイグレーションファイルを作成

---
+++ database/migrations/2023_08_24_030003_create_wallets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('team_id');
+            $table->string('name');
+            $table->bigInteger('balance')->default(0);
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
