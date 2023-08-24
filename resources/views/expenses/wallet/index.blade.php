9c6f931544eae604dd7d61c3496622f140af0aa4
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-24 09:53:12 +0900

    fix: モーダルのデザインを修正

--- resources/views/expenses/wallet/index.blade.php
+++ resources/views/expenses/wallet/index.blade.php
@@ -28,12 +28,9 @@
                                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $wallet->balance }}
                                     <a href="{{ route('wallet.edit', $wallet->id) }}"
                                         class="px-4 py-2 ml-4 text-white bg-blue-600 rounded hover:bg-blue-700">{{ __('Edit') }}</a>
-                                    <form action="{{ route('wallet.destroy', $wallet->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit"
-                                            onclick="return confirm('Are you sure you want to delete this wallet?');">Delete</button>
-                                    </form>
+                                    <button onclick="showDeleteModal('{{ route('wallet.destroy', $wallet->id) }}')"
+                                        class="px-4 py-2 ml-4 text-white bg-red-600 rounded hover:bg-red-700">{{ __('Delete') }}</button>
+
                                 </td>
 
                             </tr>
@@ -43,4 +40,57 @@
             </div>
         </div>
     </div>
+
+    <!-- Wallet Deletion Confirmation Modal -->
+    <div id="walletDeleteModal"
+        class="opacity-0 hidden fixed inset-0 z-40 w-full h-full transition-opacity duration-300 bg-black bg-opacity-50">
+        <div class="relative p-6 mx-auto mt-20 text-left bg-white border-0 rounded-lg w-96 bottom-0">
+            <!-- Modal Content -->
+            <div>
+                <h3 class="text-xl font-bold">{{ __('Delete Wallet') }}</h3>
+            </div>
+            <div class="mt-3">
+                <p class="text-sm text-gray-500">
+                    {{ __('Are you sure you want to delete this wallet? This action cannot be undone.') }}
+                </p>
+            </div>
+            <div class="flex justify-between mt-5">
+                <button onclick="toggleWalletDeleteModal()"
+                    class="px-4 py-2 text-gray-500 border border-gray-300 rounded-lg hover:bg-gray-100">{{ __('Cancel') }}</button>
+                <form id="walletDeleteForm" method="POST" action="" class="flex gap-4">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700">
+                        {{ __('Delete') }}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script>
+        function toggleWalletDeleteModal() {
+            const modal = document.getElementById('walletDeleteModal');
+            if (modal.classList.contains('hidden')) {
+                modal.classList.remove('hidden');
+                setTimeout(() => {
+                    modal.classList.remove('opacity-0');
+                }, 100);
+            } else {
+                modal.classList.add('opacity-0');
+                modal.addEventListener('transitionend', () => {
+                    if (modal.classList.contains('opacity-0')) {
+                        modal.classList.add('hidden');
+                    }
+                }, {
+                    once: true
+                });
+            }
+        }
+
+        function showDeleteModal(route) {
+            document.getElementById('walletDeleteForm').action = route;
+            toggleWalletDeleteModal();
+        }
+    </script>
+
 </x-app-layout>
